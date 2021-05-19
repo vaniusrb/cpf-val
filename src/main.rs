bee1bfae857b8fbf1e6fcb5022e4f514f7fda9aa
vanius.rb <vanius@gmail.com>
2021-05-18 23:39:30 -0300

    Otimização de build e simplificada leitura em buffer

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,66 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
 fn main() -> std::io::Result<()> {
     println!("Iniciando...");
 
-    let mut reader = my_reader::BufReader::open("res/cpfs.txt")?;
+    let file = File::open("res/cpfs.txt")?;
+    let mut reader = BufReader::new(file);
 
+    let mut cpf_buffer = [0u32; 11];
     let mut validos = 0;
     let mut invalidos = 0;
 
-    let mut buffer = String::new();
-    let mut cpf_num = [0i32; 11];
-    while let Some(line) = reader.read_line(&mut buffer) {
-        if valida_cpf_str(line?, &mut cpf_num) {
+    let mut line_buffer = String::new();
+
+    while reader.read_line(&mut line_buffer)? > 0 {
+        if valida_cpf_str(&line_buffer, &mut cpf_buffer) {
             validos += 1;
         } else {
             invalidos += 1;
         }
+        line_buffer.clear();
     }
+
     println!("Terminado. Válidos {} inválidos {}", validos, invalidos);
     Ok(())
 }
 
-fn valida_cpf_str(cpfs_reg: &str, cpf_num: &mut[i32; 11]) -> bool {
+fn valida_cpf_str(cpfs_reg: &str, cpf_buffer: &mut [u32; 11]) -> bool {
     let mut i = 0;
     for c in cpfs_reg.chars() {
         if let Some(d) = c.to_digit(10) {
-            cpf_num[i] = d as i32;
+            cpf_buffer[i] = d;
             i += 1;
         }
-    };
-    valida_cpf(&cpf_num)
-}
-
-fn valida_cpf(cpf_num: &[i32; 11]) -> bool {
-    let (d1, d2) = calcula_digitos(&cpf_num);
-    if cpf_num[9] != d1 {
-        print!("primeiro inválido {}\n", d1);
+    }
+    let (d1, d2) = calcula_digitos(&cpf_buffer);
+    if cpf_buffer[9] != d1 {
+        println!("primeiro inválido {}", d1);
         false
-    } else if cpf_num[10] != d2 {
-        print!("segundo inválido {}\n", d2);
+    } else if cpf_buffer[10] != d2 {
+        println!("segundo inválido {}", d2);
         false
     } else {
         true
     }
 }
 
-fn calcula_digitos(cpf_num: &[i32; 11]) -> (i32, i32) {
-    let mut summ = 0;
+fn calcula_digitos(cpf_buffer: &[u32; 11]) -> (u32, u32) {
+    let mut sum = 0;
     for i in 0..9 {
-        summ += cpf_num[i] * (10 - i as i32)
+        sum += cpf_buffer[i] * (10 - i as u32)
     }
-    let mut remain = summ % 11;
+    let mut remain = sum % 11;
     let d1 = if remain > 1 { 11 - remain } else { 0 };
 
-    summ = 0;
+    sum = 0;
     for i in 0..10 {
-        summ += cpf_num[i] * (11 - i as i32)
+        sum += cpf_buffer[i] * (11 - i as u32)
     }
-    remain = summ % 11;
+    remain = sum % 11;
     let d2 = if remain > 1 { 11 - remain } else { 0 };
 
     (d1, d2)
-}
-
-mod my_reader {
-    use std::{
-        fs::File,
-        io::{self, prelude::*},
-    };
-
-    pub struct BufReader {
-        reader: io::BufReader<File>,
-    }
-
-    impl BufReader {
-        pub fn open(path: impl AsRef<std::path::Path>) -> io::Result<Self> {
-            let file = File::open(path)?;
-            let reader = io::BufReader::new(file);
-
-            Ok(Self { reader })
-        }
-
-        pub fn read_line<'buf>(
-            &mut self,
-            buffer: &'buf mut String,
-        ) -> Option<io::Result<&'buf mut String>> {
-            buffer.clear();
-
-            self.reader
-                .read_line(buffer)
-                .map(|u| if u == 0 { None } else { Some(buffer) })
-                .transpose()
-        }
-    }
 }
